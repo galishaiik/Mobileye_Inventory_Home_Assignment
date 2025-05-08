4dce48f7ff9cb5f53671dd5d2274ca47b48646ba
Galishaiik <galishaiik@gmail.com>
2025-05-08 12:48:49 +0300

    def filled_inventory(): fixture declaration

--- conftest.py
+++ conftest.py
@@ -4,3 +4,13 @@ from Mobileye_Classes import Inventory
 @pytest.fixture
 def empty_inventory():
     return Inventory()
+
+@pytest.fixture
+def filled_inventory():
+    inventory = Inventory()
+    inventory.products = {
+        "Banana": 15,
+        "Apple": 25,
+        "pineapple": 37.2
+    }
+    return inventory
