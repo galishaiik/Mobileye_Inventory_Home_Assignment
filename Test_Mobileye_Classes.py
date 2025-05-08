8944af3aea49d9699111b247b0a7727f0b4baf2e
Galishaiik <galishaiik@gmail.com>
2025-05-08 12:37:18 +0300

    def test_inventory_add_product(empty_inventory)->test add product function

--- Test_Mobileye_Classes.py
+++ Test_Mobileye_Classes.py
@@ -1,1 +1,19 @@
 from Mobileye_Classes import Product
+
+def test_inventory_add_product(empty_inventory):
+    inventory = empty_inventory
+
+    # Verify no items inside inventory dict
+    assert inventory.get_inventory_products_status() == {}
+
+    # Add a new product
+    product1 = Product(name="Apple", amount=10)
+    updated_status = inventory.add_product(product1)
+    assert updated_status == {"Apple": 10}
+    assert inventory.get_inventory_products_status() == {"Apple": 10}
+
+    # Add more of the same product
+    product2 = Product(name="Apple", amount=5)
+    updated_status = inventory.add_product(product2)
+    assert updated_status == {"Apple": 15}
+    assert inventory.get_inventory_products_status() == {"Apple": 15}
