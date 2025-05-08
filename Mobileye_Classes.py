e65e04d8a3bf2031fec13c2f06ba7fc6c3d9bc9b
Galishaiik <galishaiik@gmail.com>
2025-05-08 12:09:11 +0300

    def add_product(self, product: Product)

--- Mobileye_Classes.py
+++ Mobileye_Classes.py
@@ -9,3 +9,10 @@ class Inventory:
 
     def get_inventory_products_status(self):
         return self.products
+
+    def add_product(self, product: Product):
+        if product.name in self.products.keys():
+            self.products[product.name] = self.products[product.name] + product.amount
+        else:
+            self.products[product.name] = product.amount
+        return self.products
